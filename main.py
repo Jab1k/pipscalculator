76be2720f32d0209daa5b0a466dfd306e4911a32
Jab1k <jobir4166@gmail.com>
2024-10-07 12:00:47 +0500

    BTC/USD - add

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def calculate_pips(entry_price, exit_price, pair):
     Args:
         entry_price (float): The price at which the trade was opened.
         exit_price (float): The price at which the trade was closed.
-        pair (str): Currency pair (e.g., 'EURUSD', 'USDJPY', 'XAUUSD').
+        pair (str): Currency pair (e.g., 'EURUSD', 'USDJPY', 'XAUUSD', 'BTCUSD').
 
     Returns:
         float: The number of pips gained or lost.
@@ -16,8 +16,8 @@ def calculate_pips(entry_price, exit_price, pair):
     # Define pip values based on the currency pair
     if "JPY" in pair:
         pip_value = 0.01
-    elif "XAU" in pair:
-        pip_value = 0.01
+    elif "XAU" in pair or "BTC" in pair:
+        pip_value = 0.01  # Similar pip value for Gold and Bitcoin
     else:
         pip_value = 0.0001
 
@@ -34,7 +34,7 @@ def calculate_pip_value(pips, lot_size, pair, exchange_rate=1.0):
     Args:
         pips (float): The number of pips gained or lost.
         lot_size (float): The lot size of the trade.
-        pair (str): Currency pair (e.g., 'EURUSD', 'USDJPY', 'XAUUSD').
+        pair (str): Currency pair (e.g., 'EURUSD', 'USDJPY', 'XAUUSD', 'BTCUSD').
         exchange_rate (float): Exchange rate to convert to the account's base currency. Default is 1.0.
 
     Returns:
@@ -45,6 +45,8 @@ def calculate_pip_value(pips, lot_size, pair, exchange_rate=1.0):
         pip_value_per_lot = 1000  # For JPY pairs
     elif "XAU" in pair:
         pip_value_per_lot = 1  # For XAU/USD, $1 per pip for standard lot
+    elif "BTC" in pair:
+        pip_value_per_lot = 0.01  # For BTC/USD, $0.01 per pip for standard lot
     else:
         pip_value_per_lot = 10  # For other pairs
 
@@ -58,9 +60,10 @@ def main():
     try:
         xauusd = getGoldPrice()
         entry = float(input("Kirish zonani kiriting: "))
-        exit = float(input(f"Chiqishzonasini kiriting (xozirda: {xauusd["Last Trade"]}): "))
-        pair = input("Qaysi platformada ishlayapsiz ? (Misol uchun:EURUSD, USDJPY, XAUUSD): ").upper()
+        exit = float(input(f"Chiqish zonasini kiriting (xozirda XAUUSD narxi: {xauusd['Last Trade']}): "))
+        pair = input("Qaysi platformada ishlayapsiz ? (Misol uchun: EURUSD, USDJPY, XAUUSD, BTCUSD): ").upper()
         lot_size = float(input("Necha lotda (e.g., 0.1, 1.0): "))
+        
         # Calculate the pips
         pips = calculate_pips(entry, exit, pair)
         print(f"Pips: {pips} pips")
